2178c1ef35efae5c34ba24d489cf0937e7407b47
Koji Kawamura <ijokarumawak@gmail.com>
2018-09-07 17:01:05 +0900

    Disable js minifi profile.

--- build-aliases.sh
+++ build-aliases.sh
@@ -10,6 +10,7 @@ alias nifi-deploy-properties="cp \
 .//nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-properties-loader/target/nifi-properties-loader-${NIFI_VER}.jar \
 \$NIFI_SS_HOME/lib"
 
+alias nifi-build-framework="mvn -P\!minify-and-compress install -DskipTests -f nifi-nar-bundles/nifi-framework-bundle"
 alias nifi-build-framework-api="mvn --projects nifi-framework-api install -DskipTests"
 alias nifi-deploy-framework-api="cp -p nifi-framework-api/target/nifi-framework-api*.jar \$NIFI_SS_HOME/lib"
 
@@ -49,7 +50,7 @@ nifi-nar-bundles/nifi-framework-bundle/nifi-framework-nar install -DskipTests"
 
 alias nifi-build-web-ui="mvn --projects \
 nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-ui,\
-nifi-nar-bundles/nifi-framework-bundle/nifi-framework-nar install -DskipTests"
+nifi-nar-bundles/nifi-framework-bundle/nifi-framework-nar install -DskipTests -P\!minify-and-compress"
 
 alias nifi-build-docs="mvn --projects \
 nifi-docs,\
@@ -57,6 +58,7 @@ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-docs,\
 nifi-nar-bundles/nifi-framework-bundle/nifi-framework-nar install -T4 -DskipTests"
 
 alias nifi-build-standard="mvn --projects \
+nifi-nar-bundles/nifi-extension-utils/nifi-processor-utils,\
 nifi-nar-bundles/nifi-standard-bundle/nifi-standard-processors,\
 nifi-nar-bundles/nifi-standard-bundle/nifi-standard-content-viewer,\
 nifi-nar-bundles/nifi-standard-bundle/nifi-standard-nar install -DskipTests"
@@ -83,9 +85,8 @@ nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundl
 "
 
 alias nifi-build-couchbase="mvn --projects \
-nifi-nar-bundles/nifi-couchbase-bundle/nifi-couchbase-service-api,\
-nifi-nar-bundles/nifi-couchbase-bundle/nifi-couchbase-service-api-nar,\
-nifi-nar-bundles/nifi-couchbase-bundle/nifi-couchbase-services,\
+nifi-nar-bundles/nifi-couchbase-bundle/nifi-couchbase-services-api,\
+nifi-nar-bundles/nifi-couchbase-bundle/nifi-couchbase-services-api-nar,\
 nifi-nar-bundles/nifi-couchbase-bundle/nifi-couchbase-processors,\
 nifi-nar-bundles/nifi-couchbase-bundle/nifi-couchbase-nar install -DskipTests"
 
